ba6805b92fa1b79884ecb496ba4705a6ed6d85f4
Fedi <fedijou@gmail.com>
2022-04-28 06:22:30 +0100

    gsap animation

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React , {useRef ,useEffect} from 'react'
 import people from '../../assets/people.png';
 import ai from '../../assets/ai.png';
 import "./header.css"
+import { TimelineLite, Power2 } from "gsap";
+import CSSRulePlugin from "gsap/CSSRulePlugin";
 
 const Header = () => {
+  let container = useRef(null);
+  let image = useRef(null);
+  let imageReveal = CSSRulePlugin.getRule(".gpt3__header-image:after");
+
+  let tl = new TimelineLite();
+
+  useEffect(() => {
+    tl.to(container, 0, { css: { visibility: "visible" } });
+    tl.to(imageReveal, 2.5, { width: "0%", ease: Power2.easeInOut });
+    tl.from(image, 1.4, {
+      scale: 1,
+      ease: Power2.easeInOut,
+      delay: -1.4
+    } , []);
+  });
   return (
     <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
@@ -16,13 +33,13 @@ const Header = () => {
       </div>
 
       <div className="gpt3__header-content__people">
-        <img src={people} />
+        <img src={people} alt="people" />
         <p>1,600 people requested access a visit in last 24 hours</p>
       </div>
     </div>
 
-    <div className="gpt3__header-image">
-      <img src={ai} />
+    <div className="gpt3__header-image" ref = {ell => container = ell }>
+      <img src={ai} alt="header" ref = {el => image = el }  /> 
     </div>
   </div>
   )
